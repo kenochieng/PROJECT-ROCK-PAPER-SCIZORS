f90aee3f9fd6e23c1cc05d289ecb3adb66eb390a
Ken Ochieng <kenochiengstudy@gmail.com>
2021-08-10 12:08:54 +0300

    I just added my fist files(Skeleton...flesh coming soon)

---
+++ script.js
@@ -0,0 +1,21 @@
+/* STEP1: create a function computerPlay to
+generate a random answer from the three choices
+"Rock,Paper or Scssors" */
+
+
+/* STEP2: Write a function that plays a single round of Rock Paper Scissors
+The function should take two parameters - the playerSelection and 
+computerSelection - and then return a string that declares the 
+winner of the round like so: "You Lose! Paper beats Rock" */
+
+/* STEP3: Make your function’s playerSelection parameter case-insensitive
+ (so users can input rock, ROCK, RocK or any other variation).*/
+
+ /*NOTE THAT: Important note: you want to return the results of this function call,
+  not console.log() them. To test this function console.log the results: */
+
+
+
+  /*STEP4: Write a NEW function called game(). Use the previous function inside of 
+  this one to play a 5 round game that keeps score and reports
+   a winner or loser at the end. call function 5 times or use loop */
